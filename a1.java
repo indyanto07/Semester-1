af4e07430562bebe8876f74b3e6338f4db26a08f
Andrian Indyanto <indyanto07@gmail.com>
2026-01-11 23:29:22 +0700

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,15 @@
+package ououpee;
+
+public class a1 {
+   
+        String merk;
+        String warna;
+
+        public void jalan(){
+            System.out.println("Mobil sedang berjalan");
+        }
+    }
+
+    
+
+
