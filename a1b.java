59e35f68a51ec03733b61e8035dc672e04af1fe2
Andrian Indyanto <indyanto07@gmail.com>
2026-01-11 23:29:35 +0700

    Add files via upload

---
+++ a1b.java
@@ -0,0 +1,19 @@
+package ououpee;
+
+
+
+public class a1b {
+     
+        public static void main(String[] args) {
+            a1 m1 = new a1();
+            m1.merk ="toyota";
+            m1.warna = "merah";
+
+            System.out.println("Merk mobil "+m1.merk);
+            System.out.println("Merk mobil "+m1.warna);
+            m1.jalan();
+        }
+    
+        
+    }
+
