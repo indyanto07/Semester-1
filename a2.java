596266366d2b28f3cd51a93cc6509a3fa8c0261d
Andrian Indyanto <indyanto07@gmail.com>
2026-01-14 23:23:33 +0700

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,13 @@
+package ououpee;
+
+public class a2 {
+    private String nama;
+
+    public void setNama(String namaBaru){
+        nama = namaBaru;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+}
